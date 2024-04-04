ab16f163ac30f071eb370a7957dee64bba4ed7aa
julcontrerass <111258374+julcontrerass@users.noreply.github.com>
2024-04-04 20:21:33 -0300

    add activity 5, 6 and 7

--- Guia2.cpp
+++ Guia2.cpp
@@ -95,3 +95,86 @@ int main()
 
     return 0;
 }*/
+
+/* EJERCCIO 5
+Un negocio de perfumería efectúa descuentos según el importe de la venta.
+Si el importe es menor a $100 aplicar un descuento del 5%
+Si el importe es entre $100 y hasta $500 aplicar un descuento del 10%
+Si el importe es mayor a $500 aplicar un descuento del 15%Hacer un programa donde se ingresa el importe original sin descuento y se informe por pantalla el importe con el descuento ya aplicado.
+Importante: Verifique que el programa emita UN SOLO CARTEL.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Precio;
+
+    cout << "Ingrese el importe: ";
+    cin >> Precio;
+
+    if (Precio < 100){
+        int Descuento = Precio - (Precio * 0.5);
+        cout << "El precio final es del: " << Descuento;
+    }else if (Precio>=100 && Precio<=500){
+        int Descuento = Precio - (Precio * 0.10);
+        cout << "El precio final es del: " << Descuento;
+    }else {
+        int Descuento= Precio - (Precio*0.15);
+        cout << "El importe total es del: " << Descuento;
+    }
+    return 0;
+}*/
+/* EJERCICIO6:
+Hacer un programa para ingresar por teclado tres números y luego determinar e informar con una leyenda aclaratoria si los tres son iguales entre sí, caso contrario no emitir nada.
+Tener en cuenta:  Si A es igual a B y B es igual a C, entonces A y C son iguales.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num1,Num2,Num3;
+    cout << "Ingrese 3 numeros: ";
+    cout << "Primer numero: ";
+    cin >> Num1;
+    cout << "Segundo numero: ";
+    cin >> Num2;
+    cout << "Tercer numero: ";
+    cin >> Num3;
+
+    if(Num1 == Num2 && Num2 == Num3){
+       cout << "Los tres numeros ingresados son iguales entre si";
+    }
+
+    return 0;
+}*/
+
+/*EJERCICIO 7:
+Hacer un programa para ingresar por teclado tres números e informar con una leyenda aclaratoria si los tres son todos distintos entre sí, caso contrario no emitir nada.
+Tener en cuenta: Si A es distinto de B y B es distinto de C, eso no significa que A y C sean distintos. Ejemplo: A=8, B=6 y C=8.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num1, Num2, Num3;
+    cout << "Ingrese tres numeros : ";
+    cout << "Numero 1: ";
+    cin >> Num1;
+    cout << "Numero 2: ";
+    cin >> Num2;
+    cout << "Numero 3: ";
+    cin >> Num3;
+
+    if (Num1 != Num2 && Num2 != Num3 && Num1 != Num3) {
+        cout << "Los tres numeros son diferentes entre si";
+    }
+
+    return 0;
+}*/
+
